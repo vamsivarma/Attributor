266250aa632706d5adc3aea38785cad5ab857558
Vamsi Krishna Varma <vamsi.gunturi@gmail.com>
2014-09-17 11:48:04 +0530

    Auto Font Adjust
    Changing the fonts of the text based on the fonts changes through
    setting
    
    Implementation of NSNotifications

--- ViewController.m
+++ ViewController.m
@@ -28,6 +28,35 @@
     [self.outlineBtn setAttributedTitle:title forState:UIControlStateNormal];
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil
+     ];
+}
+
+- (void) preferredFontsChanged :(NSNotification *)notification {
+    [self usePreferredFonts];
+}
+
+- (void) usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+-(void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range: self.body.selectedRange];
 }
